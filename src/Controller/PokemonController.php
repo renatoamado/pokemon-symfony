19942d90025f6b6dcc5e936912b8e7250e19202e
renatoamado <rsfuuton@gmail.com>
2025-01-28 10:37:49 -0300

    feature(ssl cert): installed  ssl certificates (auto-signed) to enable https locally and added CSP headers

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -6,6 +6,7 @@ use App\Service\PokemonService;
 use Exception;
 use Pagerfanta\Adapter\ArrayAdapter;
 use Pagerfanta\Pagerfanta;
+use Random\RandomException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,8 +14,14 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class PokemonController extends AbstractController
 {
+    private string $nonce;
+
+    /**
+     * @throws RandomException
+     */
     function __construct(private readonly PokemonService $service)
     {
+        $this->nonce = base64_encode(random_bytes(16));
     }
 
     #[Route('/pokemon', name: 'pokemon_list', methods: ['GET'])]
@@ -22,22 +29,28 @@ final class PokemonController extends AbstractController
     {
         try {
             $cards = $this->service->findAll();
+
+            $adapter = new ArrayAdapter($cards);
+            $pagerfanta = new Pagerfanta($adapter);
+            $pagerfanta->setMaxPerPage(20);
+            $pagerfanta->setCurrentPage((int)$request->query->get('page', 1));
+
+            $response = $this->render('pokemon/index.html.twig', [
+                'cards' => $pagerfanta->getCurrentPageResults(),
+                'pager' => $pagerfanta,
+                'error' => null,
+                'nonce' => $this->nonce,
+            ]);
         } catch (Exception $exception) {
-            return $this->render('pokemon/index.html.twig', [
+            $response = $this->render('pokemon/index.html.twig', [
                 'error' => $exception->getMessage(),
+                'nonce' => $this->nonce,
             ]);
         }
 
-        $adapter = new ArrayAdapter($cards);
-        $pagerfanta = new Pagerfanta($adapter);
-        $pagerfanta->setMaxPerPage(20);
-        $pagerfanta->setCurrentPage((int)$request->query->get('page', 1));
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-{$this->nonce}' 'strict-dynamic'; style-src 'self'; img-src 'self' data: https://images.pokemontcg.io; font-src 'self'; frame-ancestors 'self'; form-action 'self';");
 
-        return $this->render('pokemon/index.html.twig', [
-            'cards' => $pagerfanta->getCurrentPageResults(),
-            'pager' => $pagerfanta,
-            'error' => null,
-        ]);
+        return $response;
     }
 
     #[Route('/pokemon/show/{id}', name: 'pokemon_profile', methods: ['GET'])]
@@ -45,15 +58,21 @@ final class PokemonController extends AbstractController
     {
         try {
             $card = $this->service->findById($id);
+
+            $response = $this->render('pokemon/show.html.twig', [
+                'card' => $card,
+                'error' => null,
+                'nonce' => $this->nonce,
+            ]);
         } catch (Exception $exception) {
-            return $this->render('pokemon/show.html.twig', [
+            $response = $this->render('pokemon/show.html.twig', [
                 'error' => $exception->getMessage(),
+                'nonce' => $this->nonce,
             ]);
         }
 
-        return $this->render('pokemon/show.html.twig', [
-            'card' => $card,
-            'error' => null,
-        ]);
+        $response->headers->set('Content-Security-Policy', "default-src 'self'; script-src 'self' 'nonce-{$this->nonce}' 'strict-dynamic'; style-src 'self'; img-src 'self' data: https://images.pokemontcg.io; font-src 'self'; frame-ancestors 'self'; form-action 'self';");
+
+        return $response;
     }
 }
